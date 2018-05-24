75dcfc38c0438bc462bb4c5f5001abd5c1233a8e
Daniel Pérez Rubio <danielperezr88@gmail.com>
2018-05-24 09:50:39 +0200

    local-graph branch created

--- TFWorker-API.py
+++ TFWorker-API.py
@@ -1,23 +1,23 @@
-from os import getpid, path
+from os import getpid#, path
 import inspect
 import logging
 import tornado.ioloop
 from tornado_json.routes import get_routes
 from tornado_json.application import Application
 
-from google.protobuf import timestamp_pb2
-from gcloud import storage
+#from google.protobuf import timestamp_pb2
+#from gcloud import storage
 
 import TFWorker
 
-CACHE_BUCKET = 'cache-files'
+#CACHE_BUCKET = 'cache-files'
 
 
-def lookup_bucket(cli, prefix):
-    for bucket in cli.list_buckets():
-        if bucket.name.startswith(prefix):
-            return bucket.name
-    logging.error("Cache Bucket not found")
+#def lookup_bucket(cli, prefix):
+#    for bucket in cli.list_buckets():
+#        if bucket.name.startswith(prefix):
+#            return bucket.name
+#    logging.error("Cache Bucket not found")
 
 
 def save_pid():
@@ -35,15 +35,15 @@ if __name__ == '__main__':
 
     save_pid()
 
-    for filename in ['retrained_graph.pb', 'retrained_labels.txt', 'breakhis_retrained_graph.pb',
-                     'breakhis_retrained_labels.txt']:
-        filepath = path.join(path.dirname(path.realpath(__file__)), filename)
-        if not path.exists(filepath):
-            client = storage.Client()
-            blob = client.get_bucket(lookup_bucket(client, CACHE_BUCKET)).get_blob(filename)
-            fp = open(filepath, 'wb')
-            blob.download_to_file(fp)
-            fp.close()
+    #for filename in ['retrained_graph.pb', 'retrained_labels.txt', 'breakhis_retrained_graph.pb',
+    #                 'breakhis_retrained_labels.txt']:
+    #    filepath = path.join(path.dirname(path.realpath(__file__)), filename)
+    #    if not path.exists(filepath):
+    #        client = storage.Client()
+    #        blob = client.get_bucket(lookup_bucket(client, CACHE_BUCKET)).get_blob(filename)
+    #        fp = open(filepath, 'wb')
+    #        blob.download_to_file(fp)
+    #        fp.close()
 
     routes = get_routes(TFWorker)
 
